a8b2a9898644ecf20f8ff477107fdc20ea3e7b75
Jenny Östman <jenny.ostman@yh.nackademin.se>
2020-04-23 15:49:40 +0200

    first commit

---
+++ src/main/java/exarb/fmgamificationlogic/FmgamificationlogicApplication.java
@@ -0,0 +1,13 @@
+package exarb.fmgamificationlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmgamificationlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmgamificationlogicApplication.class, args);
+	}
+
+}
